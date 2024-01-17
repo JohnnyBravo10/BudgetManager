10613476a152f87077ef98b432d4a9448148aaa6
Johnny <giovannizanin00@gmail.com>
2024-01-17 18:27:17 +0100

    Primo commit con la mia cartella

---
+++ app/app.js
@@ -0,0 +1,372 @@
+const express = require('express');
+const fs = require('fs/promises');
+const { MongoClient } = require('mongodb');
+const session = require('express-session');
+const uri = 'mongodb://mongo:27017';
+const app = express();
+
+const client = new MongoClient(uri);
+let db = null;
+
+app.use(express.static(`${__dirname}/public`));
+app.use(express.json());
+app.use(express.urlencoded());/////////////dubbio
+app.use(session({
+    secret: 'segreto',
+    resave: false
+}));
+
+app.post('/api/auth/signin', async (req, res) => {  	
+    //const client = new MongoClient(uri);
+    //await client.connect();
+    //const users = client.db("users");
+    const db_user = await db.collection("users").findOne({username: req.body.username});
+    if(db_user && db_user.password === req.body.password){
+        req.session.username = req.body.username;
+        res.status(200).send({ message: 'Autenticazione avvenuta con successo' });
+    } else {
+        res.status(401).send("Non autenticato!");
+    }
+});
+
+app.post('/api/auth/signup', async (req, res) => {  	
+    //const client = new MongoClient(uri);
+    //await client.connect();
+    //const users = client.db("users");
+    const db_user = await db.collection("users").findOne({username: req.body.username});
+    if(db_user){
+        res.status(401).send("Username già esistente");
+    }
+    else{
+        await db.collection("users").insertOne({username: req.body.username, password: req.body.password, firstName: req.body.firstName, lastName: req.body.lastName});
+        req.session.username = req.body.username;
+      
+        res.status(200).send({ message: 'Registrazione avvenuta con successo' });
+    }
+});
+
+function verify(req, res, next){
+    if(req.session.username){
+        next();
+    } else {
+        //res.status(401).send("Non autenticato");
+        res.json({authenticated: false});////////////////////////
+    }
+}
+
+
+
+
+
+
+
+
+app.get("/api/budget/:year/:month/:id", verify, async (req, res) => {
+    let year = parseInt(req.params.year);
+    let month = parseInt(req.params.month);
+    let id = parseInt(req.params.id);
+    let transition = await db.collection("transitions").findOne({$or: [
+          { buyer: req.session.username },
+          { "users": { $elemMatch: { username: req.session.username } } }
+        ], year: year, month:month, id:id});
+    res.json(transition);
+});
+
+app.post("/api/budget/:year/:month", verify, async (req, res) => {
+    let date = req.body.date;
+    let description= req.body.description;
+    let category = req.body.category;
+    let totalPrice = parseFloat(req.body.totalPrice);
+    let buyer = req.session.username;
+    
+    let usernames = req.body.usernames.split(',');
+    let amount = req.body.portions.split(',').map(parseFloat)
+
+    let usernamePortionsMap = {};
+    for (let i = 0; i < usernames.length; i++) {
+      const username = usernames[i];
+      const portion = amount[i];    
+      if (!usernamePortionsMap[username]) {
+        usernamePortionsMap[username] = 0;
+      }
+      usernamePortionsMap[username] += portion;
+    }
+    let usernamesAndPortions = Object.keys(usernamePortionsMap).map(username => {
+      return { username: username, amount: usernamePortionsMap[username] };
+    });
+    usernamesAndPortions = usernamesAndPortions.filter(item => item.username && item.amount !== 0 && item.amount);
+    
+
+    let transitions = await db.collection("transitions").find().sort({ "id": -1 }).limit(1).toArray();
+    let id=0;
+    if (transitions.length > 0) {
+        id = transitions[0].id+1;
+      }
+
+    let new_expense = {
+        id: id,
+        buyer: buyer,
+        date:date,
+        description:description,
+        category:category,
+        totalPrice:totalPrice,
+        users: usernamesAndPortions    
+    };
+    await db.collection("transitions").insertOne(new_expense);
+    res.status(200).json(new_expense);
+});
+
+app.put("/api/budget/:year/:month/:id", verify, async (req, res) => {
+    //let year = parseInt(req.params.year);
+    //let month = parseInt(req.params.month);
+    //let day = parseInt(req.body.day);
+    let description= req.body.description;
+    let category = req.body.category;
+    let totalPrice = parseFloat(req.body.totalPrice);
+    let id = parseInt(req.params.id)
+    
+    let usernames = req.body.usernames.split(',');
+    let amount = req.body.portions.split(',').map(parseFloat)
+    let usernamePortionsMap = {};
+    for (let i = 0; i < usernames.length; i++) {
+      const username = usernames[i];
+      const portion = amount[i];    
+      if (!usernamePortionsMap[username]) {
+        usernamePortionsMap[username] = 0;
+      }
+      usernamePortionsMap[username] += portion;
+    }
+    let usernamesAndPortions = Object.keys(usernamePortionsMap).map(username => {
+      return { username: username, amount: usernamePortionsMap[username] };
+    });
+    usernamesAndPortions = usernamesAndPortions.filter(item => item.username && item.amount !== 0  && item.amount);
+    
+
+    await db.collection("transitions").updateOne({$or: [
+          { buyer: req.session.username },
+          { "users": { $elemMatch: { username: req.session.username } } }
+        ], id: id },
+    {$set: {
+        //year:year,
+        //month:month,
+        //day:day,
+        description:description,
+        category:category,
+        totalPrice:totalPrice,
+        users: usernamesAndPortions}
+    });
+    //const updatedExpense = await db.collection("transitions").findOne({ id: id });*/
+    res.status(200).json("ciao");//////////
+});
+
+app.delete("/api/budget/:year/:month/:id", verify, async (req, res) => {
+    let year = parseInt(req.params.year);
+    let month = parseInt(req.params.month)-1;
+    let id = parseInt(req.params.id);
+    let transition = await db.collection("transitions").deleteOne({$or: [
+          { buyer: req.session.username },
+          { "users": { $elemMatch: { username: req.session.username } } }
+        ], /*year: year, month: month*/ id:id});
+    res.json(parseInt(req.params.id));
+});
+
+app.get("/api/balance/:id", verify, async (req, res) =>{
+  const userId = req.params.id;
+  const otherUsers = await db.collection("users").find().toArray();
+  let otherUser =null;
+  otherUsers.forEach((u) => {
+    if (u._id.toString() === userId) {
+      otherUser=u;
+        }
+      });
+
+  let otherUsername = otherUser.username;
+  let username = req.session.username
+  let transitions = await db.collection("transitions").find({
+      $or: [
+        { buyer: username },
+        { "users": { $elemMatch: { username: username } } }
+      ],
+      $or: [
+        { buyer: otherUsername },
+        { "users": { $elemMatch: { username: otherUsername } } }
+      ],
+    }).toArray();
+  let debt=0
+
+  transitions.forEach((expense) => {
+  if (expense.buyer === username) {
+    let parts = expense.users;
+    parts.forEach((part) => {
+      if ((part.username == otherUsername)) {
+          debt-=part.amount;
+      }
+    });
+  } 
+
+  if (expense.buyer === otherUsername) {
+    let parts = expense.users;
+    parts.forEach((part) => {
+      if ((part.username == username)) {
+          debt+=part.amount;
+      }
+    });
+  } 
+
+    });
+  
+res.json(debt);
+} )
+
+app.get("/api/balance", verify, async (req, res) =>{
+    let username = req.session.username
+    let transitions = await db.collection("transitions").find({
+        $or: [
+          { buyer: username },
+          { "users": { $elemMatch: { username: username } } }
+        ]
+      }).toArray();
+
+    debts = [];
+
+    transitions.forEach((expense) => {
+        if (expense.buyer === username) {
+            let parts = expense.users;
+            parts.forEach((part) => {
+                if (!(part.username == username)) {
+                    let existingDebt = debts.find(debt => debt.debtor === part.username);
+
+                    if (existingDebt) {
+                        existingDebt.amount -= part.amount;
+                    } else {
+                        debts.push({ debtor: part.username, amount: -part.amount });
+                    }
+                }
+            });
+        } else {
+            let buyer = expense.buyer;
+            let parts = expense.users;
+            parts.forEach((part) => {
+                if (part.username === username) {
+                    let existingDebt = debts.find(debt => debt.debtor === buyer);
+
+                    if (existingDebt) {
+                        existingDebt.amount += part.amount;
+                    } else {
+                        debts.push({ debtor: buyer, amount: part.amount });
+                    }
+                }
+            });
+        }
+    });
+  res.json({debts: debts, authenticated:true});
+} )
+
+
+
+app.get("/api/budget/search", verify, async (req, res) => {
+  console.log("inoltrato a budget/search");
+  const query = req.query.q;
+  console.log("query arrivata:", query)
+  let trans = await db.collection("transitions").find({
+      $or: [
+          { buyer: req.session.username },
+          { "users": { $elemMatch: { username: req.session.username } } }
+      ],      
+  }).toArray();
+  t=[]
+  trans.forEach((element)=> {
+    if (element.category.startsWith(query)){
+    t.push(element)
+  }})
+  res.json({transitions: t, authenticated:true});
+  });
+
+
+app.get("/api/budget/whoami", verify, async (req,res) => {
+    console.log("Richiesta arrivata a whoami");////////////
+    const user = await db.collection("users").findOne({username: req.session.username});
+    res.json({firstName: user.firstName, lastName: user.lastName});
+});
+
+app.get("/api/users/search", verify, async (req, res) => {
+    const username = req.query.q;
+    let user = await db.collection("users").findOne({username: username});
+    res.json({user: user});
+});
+
+app.get("/api/users/startsWith", verify, async (req, res) => {
+  console.log("fetch a users/startswith arrivata al server")
+  const query = req.query.q.toLowerCase();
+  let users = await db.collection("users").find().toArray();
+  console.log("all users:",users)
+  suggestions=[]
+  users.forEach((element)=> {
+    if (element.username.toLowerCase().startsWith(query)){
+    suggestions.push(element.username)
+  }})
+  console.log("suggestions:", suggestions)
+  res.json({suggestions: suggestions});
+  
+});
+
+app.get("/api/budget/:year/:month", verify, async (req, res) => {
+  let year = Number(req.params.year);
+  let month = Number(req.params.month);
+  let trans = await db.collection("transitions").find({
+    $or: [
+        { buyer: req.session.username },
+        { "users": { $elemMatch: { username: req.session.username } } }
+    ]
+}).toArray();
+t=[]
+trans.forEach((element)=> {const d = new Date(element.date); 
+  if (d.getFullYear()==year && d.getMonth()+1==month){
+  t.push(element)
+}})
+res.json({transitions: t, authenticated:true});
+});
+
+app.get("/api/budget/:year", verify, async (req, res) => {
+  let year = Number(req.params.year);
+  let trans = await db.collection("transitions").find({
+    $or: [
+        { buyer: req.session.username },
+        { "users": { $elemMatch: { username: req.session.username } } }
+    ],      
+}).toArray();
+t=[]
+trans.forEach((element)=> {const d = new Date(element.date); 
+  if (d.getFullYear()==year){
+  t.push(element)
+}})
+res.json({transitions: t, authenticated:true});
+});
+
+app.get("/api/budget", verify, async (req, res) => {
+  let transitions = await db.collection("transitions").find({
+      $or: [
+        { buyer: req.session.username },
+        { "users": { $elemMatch: { username: req.session.username } } }
+      ]
+    }).toArray();
+  res.json({transitions:transitions, authenticated:true});
+});
+
+app.get('/logout', (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      console.error('Errore durante il logout:', err);
+      res.status(500).send('Errore durante il logout');
+    } else {
+      res.json({authenticated:false});
+    }
+  });
+});
+
+app.listen(3000, async () => {
+    console.log("adesso provo a connettermi...")
+    await client.connect();
+    console.log("connesso")
+    db = client.db("expenses");
+});
---
+++ app/public/assets/js/app.js
@@ -0,0 +1,409 @@
+const { createApp } = Vue
+
+const app = createApp({
+    data() {
+        return {
+          authenticated: false,
+
+          username: "",
+          new_username:"",
+          password:"",
+          new_password:"",
+          firstName:"",
+          lastName:"",
+
+          viewTransitionsTable: false,
+          
+          viewNewExpenseForm: false,
+          viewFilterForm: false,
+          
+          viewExpenseDetail: false,  
+          viewInfo: false,
+          viewOtherUser: false,
+          
+
+          showAlert: false,
+          alertMessage:"",
+
+          transitions:[],
+
+          date: new Date(),
+          description:"",
+          category:"",
+          totalCost:0,
+          parts: [{username:"", amount:0}],
+
+          monthFilter: (new Date()).getMonth()+1,
+          yearFilter: (new Date).getFullYear(),
+
+          expenseQuery:"",
+
+          expenseInDetail: null,
+
+          modifyExpense:false,
+
+          debts:[],
+
+          usernameToFind:"",
+          userToShow: null,
+          balanceToShow: 0,
+
+          usernameSuggestions:{},
+
+        };
+      },
+
+
+      mounted() {
+      /*
+        fetch('/api/budget')
+          .then(response => {
+            if (!response.ok) {
+              throw new Error('Errore di connessione al server');
+            }
+            return response.json();
+          })
+          .then(data => {
+            this.authenticated = data.authenticated;
+          })
+          .catch(error => {
+            console.error('Errore', error);
+            this.authenticated = false;
+          });*/
+          
+      },
+      methods: {
+
+        
+        async login() {  
+
+          const response= await fetch("/api/auth/signin", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({username: this.username, password: this.password}),
+          })
+
+          if (response.ok){
+            this.authenticated=true;
+            this.getAllExpenses();
+          }
+          else{
+            this.showAlert=true;
+            this.alertMessage="Username o password errati";
+          }
+        
+        },
+
+        async signup() {  
+          const response= await fetch("/api/auth/signup", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify( {username: this.new_username, password: this.new_password, firstName:this.firstName, lastName:this.lastName}),
+          })
+
+          if (response.ok){
+            this.authenticated=true;
+            this.username= this.new_username;
+            this.getAllExpenses();  
+          }
+
+          else{
+            this.showAlert=true;
+            this.alertMessage="Username già in uso";
+          }
+          
+        },
+
+        async getAllExpenses() {
+          const response= await(await fetch("/api/budget")).json();
+          this.authenticated= response.authenticated ;
+          this.viewTransitionsTable=true;
+          if (this.authenticated){
+            this.transitions=response.transitions;
+          }
+    
+        },
+
+        async addExpense() {
+          sumOfParts=0;
+          this.parts.forEach(element => {
+            sumOfParts+=element.amount
+          })
+          
+          
+          if (sumOfParts<=this.totalCost){
+            remaining=  this.totalCost - sumOfParts
+            if (remaining>0){
+              this.parts.push({username: this.username, amount: remaining});
+            }
+            const selectedDateObject = new Date(this.date);
+            const year = selectedDateObject.getFullYear();
+            const month = selectedDateObject.getMonth() + 1; 
+            names="";
+            amounts=""
+            this.parts.forEach(element => {
+              names+=element.username;
+              names+=',';
+              amounts+=element.amount;
+              amounts+=',';
+            })
+            const response= await fetch(`/api/budget/${year}/${month}`, {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify( {date: selectedDateObject, description: this.description, category: this.category,
+              totalPrice: this.totalCost, usernames: names, portions: amounts}),
+            })
+  
+            if (response.ok){
+              this.authenticated=true;
+              this.viewTransitionsTable= true;
+              this.viewNewExpenseForm= false;
+              this.viewInfo= false; 
+              this.getAllExpenses(); 
+            }
+
+          }
+        
+
+          else{
+            this.showAlert=true;
+            this.alertMessage="Ripartizione errata"
+          }
+          this.date=new Date();
+          this.description="";
+          this.category="";
+          this.totalCost=0;
+          this.parts= [{username:"", amount:0}];
+         
+        },
+        addPart() {
+          this.parts.push({ name: '', amount: 0 });
+        },
+        removePart(index) {
+          this.parts.splice(index, 1);
+        },
+
+        addPartToDetail() {
+          this.expenseInDetail.users.push({ username: '', amount: 0 });
+        },
+        removePartFromDetail(index) {
+          this.expenseInDetail.users.splice(index, 1);
+        },
+
+        showNewExpenseForm() {
+          this.viewNewExpenseForm=true;
+          this.viewFilterForm=false;
+        },
+
+        showFilterForm() {
+          this.viewFilterForm=true;
+          this.viewNewExpenseForm=false;
+        },
+
+        async filterTransitions(){
+          
+          if (this.yearFilter==0){
+            this.getAllExpenses();
+          }
+          else {
+            if(this.monthFilter==0){
+              year= this.yearFilter;
+              const response= await(await fetch(`/api/budget/${year}`)).json();
+              this.authenticated= response.authenticated ;
+              this.transitions=response.transitions;
+              this.viewTransitionsTable=true;
+          }
+          else{
+            year= this.yearFilter;
+            month = this.monthFilter;
+            const response= await(await fetch(`/api/budget/${year}/${month}`)).json();
+            this.authenticated= response.authenticated ;
+            this.transitions=response.transitions;
+            this.viewTransitionsTable=true;
+          }
+
+            }
+
+        this.monthFilter= (new Date()).getMonth()+1;
+        this.yearFilter= (new Date()).getFullYear();
+          
+        },
+
+        async showExpenseDetails(transaction){
+          this.expenseInDetail= transaction;
+          this.viewTransitionsTable= false;
+          this.viewExpenseDetail= true;
+          this.modifyExpense=false;
+          
+        },
+
+        closeAlert() {
+          this.showAlert=false;
+        },
+
+        backToTransactionsFromDetails(){
+          this.viewExpenseDetail=false;
+          this.viewTransitionsTable= true;
+          this.getAllExpenses();
+
+        },
+
+        formatDate(dateString) {
+          const date = new Date(dateString);
+          const day = date.getDate();
+          const month = date.getMonth() + 1; // Aggiungi 1 al mese per ottenere il formato 1-12
+          const year = date.getFullYear();
+          return `${day}/${month}/${year}`;
+      },
+
+      async deleteExpense(){
+        id= this.expenseInDetail.id;
+        date= new Date(this.expenseInDetail.Date)
+        year= date.getFullYear()
+        month = date.getMonth
+        const response= await(await fetch(`/api/budget/${year}/${month}/${id}`,{
+        method: "DELETE",
+        })).json();
+        this.backToTransactionsFromDetails();
+        this.getAllExpenses();
+
+      },
+
+      editExpense(){
+        this.modifyExpense=true;
+      },
+
+      async confirmEdit(){
+        sumOfParts=0;
+        this.expenseInDetail.users.forEach(element => {
+            sumOfParts+=element.amount
+          })
+          
+          
+          if (sumOfParts<=this.expenseInDetail.totalPrice){
+            remaining=  this.expenseInDetail.totalPrice - sumOfParts;
+            if (remaining>0){
+              this.expenseInDetail.users.push({username: this.expenseInDetail.buyer, amount: remaining});
+            }
+            const selectedDateObject = new Date(this.expenseInDetail.date);
+            const year = selectedDateObject.getFullYear();
+            const month = selectedDateObject.getMonth() + 1; 
+            const id = this.expenseInDetail.id
+            names="";
+            amounts=""
+            this.expenseInDetail.users.forEach(element => {
+              names+=element.username;
+              names+=',';
+              amounts+=element.amount;
+              amounts+=',';
+            })
+            const response= await fetch(`/api/budget/${year}/${month}/${id}`, {
+              method: "PUT",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify( {description: this.expenseInDetail.description, category: this.expenseInDetail.category,
+              totalPrice: this.expenseInDetail.totalPrice, usernames: names, portions: amounts, buyer: this.expenseInDetail.buyer}),
+            })
+  
+            if (response.ok){
+              this.authenticated=true;
+              this.viewTransitionsTable = true;
+              this.viewNewExpenseForm= false; 
+              this.modifyExpense= false;
+              this.viewFilterForm= false;
+              this.viewExpenseDetail= false;
+            }
+
+
+      }
+      else{
+        this.showAlert=true;
+        this.alertMessage="Ripartizione errata"
+      }
+      },
+
+      async showInfo(){
+        this.viewInfo=true;
+        const response= await(await fetch("/api/budget/whoami",{
+          method: "GET",
+        }
+        )).json();
+        this.firstName= response.firstName;
+        this.lastName= response.lastName;
+        const response2 = await (await fetch("/api/balance")).json();
+        this.debts= response2.debts;
+        
+
+      },
+
+      async closeInfo(){
+        this.viewInfo=false;
+      },
+
+      async logout() {
+        const response= await(await fetch("logout")).json();
+        this.authenticated= response.authenticated ;
+        this.username= "";
+        this.new_username="";
+        this.password="";
+        this.new_password="";
+        this.firstName="";
+        this.lastName="";
+
+
+      },
+
+      async searchExpenses(){
+        query = this.expenseQuery;
+        const response= await(await fetch(`/api/budget/search?q=${query}`)).json();
+        console.log("Server response for search category:", response)
+
+        this.authenticated= response.authenticated ;
+        this.transitions=response.transitions;
+        this.viewTransitionsTable=true;
+      },
+
+      async searchUser(){
+        username = this.usernameToFind;
+        const response= await(await fetch(`/api/users/search?q=${username}`)).json();
+        console.log("User trovato:", response)
+        if (response== null){
+          this.showAlert=true;
+          this.alertMessage="Utente non trovato"
+        }
+        else{
+        this.userToShow= response.user;
+        let id = this.userToShow._id.toString();
+        const response2 = await(await fetch(`/api/balance/${id}`)).json();
+        this.balanceToShow= parseFloat(response2);
+        this.viewOtherUser=true;
+        }
+      },
+
+      async suggestUsernames(query, id){
+        lowQuery= query.toLowerCase()
+        if (lowQuery.length){
+        const response= await(await fetch(`/api/users/startsWith?q=${lowQuery}`)).json();
+        console.log("fetch eseguita, response ottenuta:", response)
+        this.usernameSuggestions[id]=response.suggestions;
+        console.log("suggerimenti:", this.usernameSuggestions[id])
+        }
+        else{
+          this.usernameSuggestions[id]=[];
+        }
+
+
+      },
+
+    },
+
+
+
+}).mount("#app");
